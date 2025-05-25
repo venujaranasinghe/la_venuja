3a47e72c14b567cb18555f640ab18a68f6d111de
venujaranasinghe <it23225510@my.sliit.lk>
2025-05-25 19:55:05 +0530

    Navigation

--- la_venuja/src/components/RootLayout.jsx
+++ la_venuja/src/components/RootLayout.jsx
@@ -5,8 +5,10 @@ import React, { useEffect, useId, useRef, useState } from 'react'
 import Container from './Container';
 import Link from 'next/link';
 import Logo from './Logo';
-import {HiMenuAlt4} from "react-icons/hi";
-import {IoMdClose} from "react-icons/io";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
+import Button from './Button';
+import clsx from 'clsx';
 
 const Header = ({
     panelId,
@@ -24,13 +26,73 @@ const Header = ({
                 <Link href="/" aria-label="Home">
                     <Logo invert={invert}>la_venuja</Logo>
                 </Link>
-                <div>
-                    //31.10
+                <div className='flex items-center gap-x-8'>
+                    <Button href={"/contact"} invert={invert}>
+                        Contact Me
+                    </Button>
+                    <button
+                        ref={toggleRef}
+                        type="button"
+                        onClick={onToggle}
+                        aria-expanded={expanded.toString()}
+                        aria-controls={panelId}
+                        className={clsx(
+                            "group -m-2.5 rounded-full p-2.5 transition",
+                            invert ? "hover:bg-white/10" : "hover:bg-neutral-950/10"
+                        )}
+                        aria-label="Toggle navigation"
+                    >
+                        <Icon
+                            className={clsx(
+                                "h-6 w-6",
+                                invert
+                                    ? "fill-white group-hover:fill-neutral-200"
+                                    : "fill-neutral-950 group-hover:fill-neutral-700"
+                            )}
+                        />
+                    </button>
                 </div>
             </div>
         </Container>
     )
 }
+
+const NavigationRow = ({ children }) => {
+    return (
+      <div className="even:mt-px sm:bg-neutral-950">
+        <Container>
+          <div className="grid grid-cols-1 sm:grid-cols-2">{children}</div>
+        </Container>
+      </div>
+    );
+  };
+  
+  const NavigationItem = ({ href, children }) => {
+    return (
+      <Link
+        href={href}
+        className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 even:mt-px sm:mx-0 sm:px-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-l sm:even:border-neutral-800 sm:even:pl-16"
+      >
+        {children}
+        <span className="absolute inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+      </Link>
+    );
+  };
+  
+  const Navigation = () => {
+    return (
+      <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
+        <NavigationRow>
+          <NavigationItem href="/work">My Work</NavigationItem>
+          <NavigationItem href="/about">About Me</NavigationItem>
+        </NavigationRow>
+        <NavigationRow>
+          <NavigationItem href="/process">My Process</NavigationItem>
+          <NavigationItem href="/blog">Blog</NavigationItem>
+        </NavigationRow>
+      </nav>
+    );
+  };
 
 const RootLayoutInner = ({ children }) => {
     const panelId = useId();
@@ -73,7 +135,7 @@ const RootLayoutInner = ({ children }) => {
                         }}
                     />
                 </div>
-                {children}
+                {/* {Navigation} */}
             </header>
         </MotionConfig>
     );
